c522732ab23cec3001b225b8909d53c7862e9324
avanegas <arco527@gmail.com>
2024-09-22 13:45:06 -0500

    Add

--- examples.py
+++ examples.py
@@ -1,8 +1,5 @@
-frase = "Si la implementación es difícil de explicar, puede que sea una mala idea."
+numero = 25
 
-parte_1 = frase[:31]
-parte_2 = frase[32:]
-frase_a = parte_1.replace("difícil", "fácil")
-frase_b = parte_2.replace("mala", "buena")
+raiz = int(numero ** 0.5)
 
-print(frase_a + " " + frase_b)
+print(raiz)
