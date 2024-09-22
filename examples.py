6db3322f79a57c1bfcd0f1e77df485e7e0ff7156
avanegas <arco527@gmail.com>
2024-09-22 12:20:13 -0500

    Add more results

--- examples.py
+++ examples.py
@@ -1,7 +1,8 @@
-all_name = "Este es mi nombre, Angie"
-name = "Angie"
+frase = "Si la implementación es difícil de explicar, puede que sea una mala idea."
 
-result = all_name.index(name)
+parte_1 = frase[:31]
+parte_2 = frase[32:]
+frase_a = parte_1.replace("difícil", "fácil")
+frase_b = parte_2.replace("mala", "buena")
 
-
-print(f"Este es el resultado: {result}")
+print(frase_a + " " + frase_b)
