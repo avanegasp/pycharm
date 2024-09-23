94b110e3ee04e3abf0f12d41ca52732e34e69ea7
avanegas <arco527@gmail.com>
2024-09-23 17:35:04 -0500

    Add exercises

--- examples.py
+++ examples.py
@@ -1,12 +1,6 @@
-lista_numeros = [1,5,8,7,6,8,2,5,2,6,4,8,5,9,8,3,5,4,2,5,6,4]
-suma_pares = 0
-suma_impares = 0
+respuesta = "s"
 
-for numero in lista_numeros:
-    if numero % 2 == 0:
-        suma_pares += numero
-    else:
-        suma_impares += numero
-
-    print(suma_pares)
-    print(suma_impares)
+while respuesta == 's':
+    respuesta = input('Quieres seguir? (s/n)')
+else:
+    print('gracias')
