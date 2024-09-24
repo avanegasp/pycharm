f0a4424c409bad006684bd500db4dc4b1fb6c81f
avanegas <arco527@gmail.com>
2024-09-24 18:29:30 -0500

    Add exercises

--- examples.py
+++ examples.py
@@ -1,17 +1,6 @@
-numero = 51
+valores = [1, 2, 3, 4, 5, 6, 9.5]
+valores_cuadrado = 0
 
-while numero >= 0:
-    numero = numero - 1
-    if numero % 5 == 0:
-        print(numero)
-    else:
-        numero
+valores_cuadrado = [valor*valor for valor in valores]
+print(valores_cuadrado)
 
-        lista_numeros = [4, 5, 8, 7, 6, 9, 8, 2, 4, 5, 7, 1, 9, 5, 6, -1, -5, 6, -6, -4, -3]
-
-        for number in lista_numeros:
-            if number > 0:
-                print(number)
-            else:
-                number
-                break
