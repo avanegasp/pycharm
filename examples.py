632c608121fda6fd3fe63424bb8ddbebc83a8cb3
avanegas <arco527@gmail.com>
2024-09-24 10:48:19 -0500

    Add exercises

--- examples.py
+++ examples.py
@@ -1,5 +1,17 @@
-numero = 10
+numero = 51
 
-while numero > 0:
-    print("hola", numero)
+while numero >= 0:
     numero = numero - 1
+    if numero % 5 == 0:
+        print(numero)
+    else:
+        numero
+
+        lista_numeros = [4, 5, 8, 7, 6, 9, 8, 2, 4, 5, 7, 1, 9, 5, 6, -1, -5, 6, -6, -4, -3]
+
+        for number in lista_numeros:
+            if number > 0:
+                print(number)
+            else:
+                number
+                break
